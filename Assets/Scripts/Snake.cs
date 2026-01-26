1dceadd276614b5a8355be3d3e7133530d435248
Fan-tome14 <96857821+Fan-tome14@users.noreply.github.com>
2026-01-25 19:16:33 -0500

    addcollision and sprite

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -1,42 +1,76 @@
+﻿using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine;
 
 public class Snake : MonoBehaviour
 {
-    private Vector2 _direction = Vector2.right;
+    private Vector2 direction = Vector2.right;
 
-    void Start()
+    [SerializeField] private float moveDelay = 0.25f;
+    private float timer;
+
+    [Header("Sprites de la tête")]
+    public Sprite headRight;
+    public Sprite headUp;
+    public Sprite headLeft;
+    public Sprite headDown;
+
+    private SpriteRenderer spriteRenderer;
+
+    void Awake()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = headRight; // par défaut
     }
 
-    private void Update()
+    void Update()
     {
         var keyboard = Keyboard.current;
-        if (keyboard.wKey.wasPressedThisFrame)
+
+        if (keyboard.wKey.wasPressedThisFrame && direction != Vector2.down)
         {
-            _direction = Vector2.up;
+            direction = Vector2.up;
+            spriteRenderer.sprite = headUp;
         }
-        else if (keyboard.sKey.wasPressedThisFrame)
+        else if (keyboard.sKey.wasPressedThisFrame && direction != Vector2.up)
         {
-            _direction = Vector2.down;
+            direction = Vector2.down;
+            spriteRenderer.sprite = headDown;
         }
-        else if (keyboard.aKey.wasPressedThisFrame)
+        else if (keyboard.aKey.wasPressedThisFrame && direction != Vector2.right)
         {
-            _direction = Vector2.left;
+            direction = Vector2.left;
+            spriteRenderer.sprite = headLeft;
         }
-        else if (keyboard.dKey.wasPressedThisFrame)
+        else if (keyboard.dKey.wasPressedThisFrame && direction != Vector2.left)
         {
-            _direction = Vector2.right;
+            direction = Vector2.right;
+            spriteRenderer.sprite = headRight;
         }
     }
 
-    private void FixedUpdate() 
+    void FixedUpdate()
     {
-        this.transform.position = new Vector3(
-            Mathf.Round(this.transform.position.x) + _direction.x,
-            Mathf.Round(this.transform.position.y) + _direction.y,
-            0.0f
-        );
+        timer += Time.fixedDeltaTime;
+
+        if (timer >= moveDelay)
+        {
+            transform.position = new Vector3(
+                Mathf.Round(transform.position.x) + direction.x,
+                Mathf.Round(transform.position.y) + direction.y,
+                0f
+            );
+
+            timer = 0f;
+        }
     }
-}   
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Wall"))
+        {
+            UnityEngine.Debug.Log("GAME OVER");
+            gameObject.SetActive(false);
+            Time.timeScale = 0f;
+        }
+    }
+}
